f58da72880c71c35681c1f1da0f0d2408ca48610
ELKerr <elkerr97@gmail.com>
2022-01-07 11:22:43 -0700

    Initial Commit

---
+++ war-card/war_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_cardApp.swift
+//  war-card
+//
+//  Created by Ethan L. Kerr on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct war_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
